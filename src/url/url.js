5a83bb9bed5ac95b550052c36cd8e3de6ceb9479
Frank <frigsamo@gmail.com>
2020-01-30 10:35:45 -0500

    fixing apikey

--- src/url/url.js
+++ src/url/url.js
@@ -1,8 +1,7 @@
 
 
 const baseUrl = `https://newsapi.org/v2/top-headlines?country=us&category=`;
-const apiKey = '&apiKey=d6641710c3f047f0b383f09803e3f3d7';
-//process.env.REACT_APP_API_KEY
+
  const createUrl = (categoria) => `${baseUrl}${categoria}${process.env.REACT_APP_NEW_API_KEY}`;
  export default createUrl;
 
